bd5b9a60b9ca892148d87587758d4cc70f5b97fe
EverettHanke <forgetekbusiness@gmail.com>
2025-06-08 16:12:35 -0700

    better menu

--- NoCap/src/App.jsx
+++ NoCap/src/App.jsx
@@ -1,17 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PoseTracker from './components/PoseTracker'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cameraEnabled, setCameraEnabled] = useState(false)
 
   return (
     <>
-      <div>
-        <PoseTracker />
-      </div>
+      {/* When Camera is disabled */}
+      {!cameraEnabled && (
+        <>
+          <div>
+            <h1>Welcome to NoCap</h1>
+            <p>An AI centric suitless Motion Capture alternative</p>
+            <p>Make animations for free! No Cap!</p>
+            <button
+              onClick={() => setCameraEnabled(true)}
+              sx={{
+                mt: 2,
+                px: 4,
+                py: 1,
+                backgroundColor: 'primary.main',
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: 'primary.dark'
+                }
+              }}
+            >
+              Enable Camera
+            </button>
+          </div>
+        </>
+      )}
+      {/* When Camera is enabled */}
+      {cameraEnabled && (
+        <>
+          <div>
+            <button
+              onClick={() => setCameraEnabled(false)}
+            >
+              Disable Camera
+            </button>
+            <PoseTracker />
+          </div>
+        </>
+      )}
       <p className="read-the-docs">
         Stand in full frame for the best effect
       </p>
